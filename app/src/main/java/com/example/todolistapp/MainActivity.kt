c5c39e02e54636b01eaa182f5d554b2c8b83f5dc
Alex Rosenbach <alex.rosenbach@hp.com>
2026-07-19 21:12:04 -0400

    Implement task addition logic and display task list in ToDoListScreen

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -13,8 +13,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todolistapp.ui.theme.ToDoListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,18 +37,31 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ToDoListScreen(modifier: Modifier = Modifier) {
     val taskTextState = rememberTextFieldState()
+    val tasks = remember { mutableStateListOf<String>() }
 
-    Column()
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(24.dp))
     {
-        Text(text = "Todo List", modifier = modifier)
+        Text(text = "Todo List")
         OutlinedTextField(
             state = taskTextState,
-            label = { Text("Enter a new task") })
-        Button(onClick = {}) {
+            label = { Text("Enter a new task") }
+        )
+        Button(onClick = {
+            val task = taskTextState.text.toString()
+
+            if (task.isNotBlank()) {
+                tasks.add(task)
+            }
+        }) {
             Text("Add Task")
         }
-    }
 
+        tasks.forEach { task ->
+            Text(text = task)
+        }
+    }
 }
 
 @Preview(showBackground = true)
