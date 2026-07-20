371dece62993b4eb12528eb3d03f719bd1057ef2
Alex Rosenbach <alex.rosenbach@hp.com>
2026-07-19 21:00:58 -0400

    Revert back to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.example.todolistapp"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
