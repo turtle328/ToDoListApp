cf2938b8e9f0463fb2dc2b985e5a49cbab1ada1c
Alex Rosenbach <alex.rosenbach@hp.com>
2026-07-19 20:59:40 -0400

    Implement initial ToDoListScreen UI and update compileSdk to 37

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ToDoListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ToDoListScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +32,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ToDoListScreen(modifier: Modifier = Modifier) {
+    val taskTextState = rememberTextFieldState()
+
+    Column()
+    {
+        Text(text = "Todo List", modifier = modifier)
+        OutlinedTextField(
+            state = taskTextState,
+            label = { Text("Enter a new task") })
+        Button(onClick = {}) {
+            Text("Add Task")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ToDoListScreenPreview() {
     ToDoListAppTheme {
-        Greeting("Android")
+        ToDoListScreen()
     }
 }
