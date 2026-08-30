cbde206c783b5b11010d56d4089e8c571e9be542
Alex Rosenbach <alex.rosenbach@hp.com>
2026-08-29 20:09:40 -0400

    Initialize TodoDatabase in TodoApplication

--- app/src/main/java/com/example/todolistapp/TodoApplication.kt
+++ app/src/main/java/com/example/todolistapp/TodoApplication.kt
@@ -1,6 +1,17 @@
 package com.example.todolistapp
 
 import android.app.Application
+import androidx.room3.Room
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 
 class TodoApplication : Application() {
+    val database: TodoDatabase by lazy {
+        Room.databaseBuilder(
+            this,
+            TodoDatabase::class.java,
+            "todo_database"
+        )
+            .setDriver(BundledSQLiteDriver())
+            .build()
+    }
 }
